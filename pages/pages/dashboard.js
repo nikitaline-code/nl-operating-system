38c5c090d3ae4c2ed9d7264197fabb2658c87b53
nikitaline-code <nikita.line@arrowquip.com>
2026-04-13 14:20:11 -0500

    Create dashboard.js

---
+++ pages/pages/dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to your Operating System</h1>
+      <p>This is your command center.</p>
+    </div>
+  );
+}
