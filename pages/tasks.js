24749f4c9dfbb6e9f804c467096479da76aee584
nikitaline-code <nikita.line@arrowquip.com>
2026-04-14 13:08:18 -0500

    Update tasks.js

--- pages/tasks.js
+++ pages/tasks.js
@@ -1,8 +1,3 @@
-export default function Tasks() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Daily OS</h1>
-      <p>If you see this, build works ✅</p>
-    </div>
-  );
+export default function Home() {
+  return <div>Home works ✅</div>;
 }
