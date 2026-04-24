24316fb1ddd6f0acde8c8397cf76206da69c2f49
nikitaline-code <nikita.line@arrowquip.com>
2026-04-24 14:53:53 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import "../styles/globals.css";
+import AppShell from "../components/AppShell";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppShell>
+      <Component {...pageProps} />
+    </AppShell>
+  );
 }
