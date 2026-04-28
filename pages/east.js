4ef58445b05e8cde18e3e3b1e0a829f75dd5e851
nikitaline-code <nikita.line@arrowquip.com>
2026-04-28 16:20:52 -0500

    Update east.js

--- pages/east.js
+++ pages/east.js
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useState } from "react";
 const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1AfbA0b8VKuuf8Ho2FSmzK1JH_bq1yn07umiQurWyLRW96NuQ8s-vz6M-4NKp3WFKf4fI353l2UlO/pub?gid=1945000950&single=true&output=csv";
 
 const TRADESHOW_EMBED_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1AfbA0b8VKuuf8Ho2FSmzK1JH_bq1yn07umiQurWyLRW96NuQ8s-vz6M-4NKp3WFKf4fI353l2UlO/pubhtml?gid=722935598&single=true";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1AfbA0b8VKuuf8Ho2FSmzK1JH_bq1yn07umiQurWyLRW96NuQ8s-vz6M-4NKp3WFKf4fI353l2UlO/pubhtml?gid=722935598&single=true&widget=true&headers=false";
 
 function parseCSV(text) {
   const delimiter = text.includes("\t") ? "\t" : ",";
